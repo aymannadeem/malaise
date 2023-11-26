f95532b36e4523ba95e96c8a69a9d29e239ff3b1
Ayman Nadeem <875834+aymannadeem@users.noreply.github.com>
2023-11-26 15:46:45 -0500

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information -----------------------------------------------------
+
+project = 'Malaise'
+author = 'Ayman Nadeem'
+
+# The full version, including alpha/beta/rc tags
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings.
+extensions = [
+]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and directories to ignore when looking for source files.
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.
+html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here, relative to this directory.
+html_static_path = ['_static']
